bbb79764d77d886ab150899e7442cad264a14624
Alex <Alex@gmail.com>
2016-11-11 02:17:17 +0200

    cleaning

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -15,23 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('TskMng.urls')),
 
 ]
-if settings.DEBUG:
-
-    if settings.MEDIA_ROOT:
-
-        urlpatterns += static(settings.MEDIA_URL,
-
-            document_root=settings.MEDIA_ROOT)
-
-# Эта строка опциональна и будет добавлять url'ы только при DEBUG = True
-
-urlpatterns += staticfiles_urlpatterns()
--- TskMng/views.py
+++ TskMng/views.py
@@ -1,56 +1,80 @@
 import string
 import random
-from django.shortcuts import render
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .models import Task, Progress, Prioritet, Clases
-from django.http import HttpResponseRedirect
-from django.contrib import admin
+
 # Create your views here.
+
 def task_list(request):
-            if request.user.is_anonymous:
-                return redirect('/admin')
-            circle_count = range(7)
-            counter = 1
-            flag_open = Progress.objects.get(flag = 'open')
-            flag_closed = Progress.objects.get(flag='closed')
-            flag_in_process = Progress.objects.get(flag='in process')
-            prioritet_hot = Prioritet.objects.get(prioritet='hot')
-            user =  User.objects.get(username = "Admin")
-            if request.user == user:
-                tasks_open = Task.objects.filter(task_progress=flag_open)
-                tasks_prioritet = Task.objects.filter(task_progress=flag_open, task_prioritet=prioritet_hot)
-                tasks_closed = Task.objects.filter(task_progress=flag_closed)
-                task_in_progres = Task.objects.filter(task_progress=flag_in_process)
-            else:
-                tasks_open = Task.objects.filter(task_progress = flag_open, task_author = request.user)
-                tasks_prioritet = Task.objects.filter(task_progress = flag_open, task_prioritet=prioritet_hot, task_author=request.user)
-                tasks_closed = Task.objects.filter(task_progress = flag_closed, task_author = request.user)
-                task_in_progres = Task.objects.filter(task_progress = flag_in_process, task_author = request.user)
-            return render(request, 'TskMng/task_list.html', {'prioritet_hot':prioritet_hot,
-                                                             'tasks_prioritet': tasks_prioritet,'tasks_open': tasks_open,
-                                                             'tasks_closed':tasks_closed, 'task_in_progres':task_in_progres, 'circle_count': circle_count, 'counter':counter})
+    if request.user.is_anonymous:
+        return redirect('/admin')
+    circle_count = range(7)
+    hot = Prioritet.objects.get(prioritet='hot')
+    open = get_open_tasks(request, Progress.objects.get(flag='open'))
+    closed = get_closed_tasks(request, Progress.objects.get(flag='closed'))
+    in_progres = get_task_in_process(request, Progress.objects.get(flag='in process'))
+    prioritet = get_prioritet_tasks(request, Prioritet.objects.get(prioritet='hot'), Progress.objects.get(flag='open'))
+
+    return render(request, 'TskMng/task_list.html', {'hot': hot,
+                                                     'prioritet': prioritet, 'open': open,
+                                                     'closed': closed, 'in_progres': in_progres,
+                                                     'circle_count': circle_count})
+
+
+def get_prioritet_tasks(request, hot, flag):
+    if request.user.is_superuser:
+        task = Task.objects.filter(task_progress=flag, task_prioritet=hot)
+    else:
+        task = Task.objects.filter(task_progress=flag, task_prioritet=hot, task_author=request.user)
+    return task
+
+def get_task_in_process(request, in_process):
+    if request.user.is_superuser:
+        task = Task.objects.filter(task_progress=in_process)
+    else:
+        task = Task.objects.filter(task_progress=in_process, task_author=request.user)
+    return task
+
+
+def get_open_tasks(request, open):
+    if request.user.is_superuser:
+        task = Task.objects.filter(task_progress=open)
+    else:
+        task = Task.objects.filter(task_progress=open, task_author=request.user)
+    return task
+
+
+def get_closed_tasks(request, closed):
+    if request.user.is_superuser:
+        task = Task.objects.filter(task_progress=closed)
+    else:
+        task = Task.objects.filter(task_progress=closed, task_author=request.user)
+    return task
+
 
 def task_del(request, pk):
-    task_delete = get_object_or_404(Task, pk=pk)
-    task_delete.delete()
+    delete = get_object_or_404(Task, pk=pk)
+    delete.delete()
     return redirect('task_list')
+
 
 def task_complete(request, pk):
-    flag_closed = Progress.objects.get(flag='closed')
-    task_complete = get_object_or_404(Task, pk=pk)
-    task_complete.task_progress = flag_closed
-    task_complete.save(update_fields=['task_progress'])
+    closed = Progress.objects.get(flag='closed')
+    complete = get_object_or_404(Task, pk=pk)
+    complete.task_progress = closed
+    complete.save(update_fields=['task_progress'])
     return redirect('task_list')
 
+
 def task_start(request, pk):
-    flag_in_process = Progress.objects.get(flag='in process')
-    task_start = get_object_or_404(Task, pk=pk)
-    task_start.task_progress = flag_in_process
-    task_start.save(update_fields=['task_progress'])
+    process = Progress.objects.get(flag='in process')
+    start = get_object_or_404(Task, pk=pk)
+    start.task_progress = process
+    start.save(update_fields=['task_progress'])
     return redirect('task_list')
+
 
 def auto_generator(request):
     # --------AUTO GEMERATE TASKS-----------
@@ -59,7 +83,7 @@ def auto_generator(request):
     clases = Clases.objects.all()
     auth = User.objects.all()
     counter = 0
-    for i in range(0,10):
+    for i in range(0, 10):
         my_rand_descr = ""
         for i in range(random.randint(30, 300)):
             counter += 1
@@ -69,7 +93,7 @@ def auto_generator(request):
             else:
                 my_rand_descr += "".join(random.choice(string.ascii_uppercase + string.digits))
 
-        Task.objects.create(task_author = random.choice(auth), task_name=random.choice(clases),
+        Task.objects.create(task_author=random.choice(auth), task_name=random.choice(clases),
                             task_description=my_rand_descr,
                             task_progress=random.choice(flags), task_prioritet=random.choice(prioritets),
                             task_class=random.choice(clases))
